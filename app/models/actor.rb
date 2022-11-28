10dbd23d8d479735633eedc1f761c4f8ffdea962
Yuriko <ygabriela.arnott@gmail.com>
2022-11-28 14:22:19 -0500

    commit tree adb7d3f89c2f576925de407a2aafda53af9a5f20

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+#full_name
+#returns the actor's full name 
+def full_name
+    "#{self.first_name} #{self.last_name}"
+end
+
+#list_roles
+#returns a list of roles for the actor using the character name and show name 
+def list_roles
+    self.characters.map do |char|
+        "#{char.name} - #{char.show.name}"
+    end
+end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+    #  #actors_list
+    #    returns a list of the full names of each actor associated with the show 
+    def actors_list
+        self.actors.map do |actor|
+            actor.full_name
+        end
+    end
 end
