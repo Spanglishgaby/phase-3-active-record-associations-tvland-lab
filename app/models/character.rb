2807b8a18bb6243f5e83ed4e31d93d5d8c056274
Yuriko <ygabriela.arnott@gmail.com>
2022-11-28 14:22:46 -0500

    solved

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+    #  #say_that_thing_you_say
+    #    returns a string with the character's name and catchphrase 
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
 end
